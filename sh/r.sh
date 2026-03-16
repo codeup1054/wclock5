b55978f66d42621a321d26a2cb63c2cd9e7d177e
Developer <dev@local>
2026-03-16 11:59:37 +0300

    Cleanup: remove obsolete scripts (r.sh, r_copy.sh, dep.sh, init_site.sh, test scripts)

--- sh/r.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# r.sh - Управление WClock (альтернативный скрипт)
-# Использует docker-compose как основной метод
-
-cd "$(dirname "$0")"
-
-# Запуск через docker-compose
-exec ./run_docker.sh "$@"
